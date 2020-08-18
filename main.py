aa74c9b5a7837054560772f2f8084a96794ba606
Leo Gazziro <>
2020-08-18 10:55:39 -0300

    Notificação de preço

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import requests
 import json
+import subprocess as s
+from time import sleep
+from datetime import datetime
 from lxml.html import fromstring
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 _DICT_XPATH = {
     'americanas': './/span[contains(@class, "price__SalesPrice")]',
@@ -48,6 +63,18 @@ def get_text(root, xpath):
         raise Exception("Deu erro!! {}".format(err))
 
 
+def verify_value_to_notify(value, limit, site):
+    try:
+        value_str = value
+        value = value.replace('R$', '').replace('.', '').replace(' ', '')
+        value = value.replace(',', '.')
+        value = float(value)
+        if value <= limit:
+            s.call(['notify-send', site, f'Valor abaixo do limite: {value_str}'])
+    except Exception as err:
+        print(f'Erro na conversao de valor: {err}')
+
+
 def crawler():
     """
     Função principal para extrair os textos da pagina
@@ -56,10 +83,18 @@ def crawler():
     with open('json_produto.json') as f:
         data = json.load(f)
 
-    for key in data.keys():
-        for link in data[key]:
-            site, xpath = get_xpath_from_dict(link)
-            print('Preço do site {}: {}'.format(site, get_root(link, xpath)))
+    while True:
+        print(f'Realizando consulta: {datetime.now()}')
+        for key in data.keys():
+            for link in data[key]:
+                site, xpath = get_xpath_from_dict(link)
+                value = get_root(link, xpath)
+                verify_value_to_notify(value, 9100, site)
+                print('Preço do site {}{}{}: {}{}{}'.format(bcolors.HEADER, site, bcolors.ENDC, bcolors.OKBLUE,
+                                                            value, bcolors.ENDC))
+
+        print('Aguardando!!!\n')
+        sleep(60*15)
 
 
 if __name__ == '__main__':
